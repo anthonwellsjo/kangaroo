1de9a6091b778024ccc8a50a6ec3225e25f76856
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-03-24 23:11:24 +0100

    skriva innehåll i hemsidan

--- src/pages/business.ts
+++ src/pages/business.ts
@@ -3,8 +3,12 @@ import { Page } from '../framework/framework.js';
 
 export default class BusinessPage extends Page {
   constructor() {
-    super({ pageTitle: "Business Plan", pageName: PageName.business, linkIconAbsolutePath: "src/images/business.png" });
-    this.render = () => `<p>this is the business page</p>
+    super({ pageTitle: "Affärsplan", pageName: PageName.business, linkIconAbsolutePath: "src/images/business.png" });
+    this.render = () => `
+    <p>
+      En prototyp av produkten kommer att utvecklas innan några investeringar efterfrågas. På så sätt
+      minskar vi riskerna med startkapitalet och kan göra användar-tester och förundersökningar innan lansering.
+    </p>
     `;
 
   }
