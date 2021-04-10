9cc8cd65323401a2b14abe254c6cda18064c8802
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-04-10 20:57:31 +0200

    removed git tracking on some folders

--- client/dist/src/SPA/startApp.js
+++
@@ -1,9 +0,0 @@
-import { App } from './app.js';
-import { refreshApp, Router } from './framework/framework.js';
-const startApp = () => {
-    console.log("starting");
-    const app = new App();
-    Router.configure(app);
-    refreshApp(app);
-};
-window.onload = startApp;
