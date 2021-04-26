930ae2e869bbc2d24b8532c0073567680db66270
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-04-26 13:28:16 +0200

    with axios

--- client/src/REACT/src/queries/firebase/useFirebaseUser.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-const useFirebaseUser = () => {
-  const [state, setState] = useState({ isPending: true, hasError: false, data: {} });
-
-  return {}
-}
-
-export default useFirebaseUser;
--- client/src/REACT/src/views/DashBoardView/DashBoardView.tsx
+++ client/src/REACT/src/views/DashBoardView/DashBoardView.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import LoadingScreen from '../../components/LoadingScreen/LoadingScreen';
+import useFirebaseUsers from '../../queries/firebase/useFirebaseUsers';
 
 const DashBoardView: React.FC = () => {
+
+  const { isPending, hasError, data } = useFirebaseUsers();
+  if (isPending) return <LoadingScreen />
+  if (hasError) return <h1>Error!</h1>
+  console.log("got data", data);
+
+
   return (
     <h1>Dashboard</h1>
   )
