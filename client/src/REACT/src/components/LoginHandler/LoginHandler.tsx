df874ee823d2ebf97c6e59f913a4765e58297451
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-05-14 22:17:08 +0200

    dags att sova

--- client/src/REACT/src/components/LoginHandler/LoginHandler.tsx
+++ client/src/REACT/src/components/LoginHandler/LoginHandler.tsx
@@ -5,6 +5,9 @@ import { useQuery } from '@apollo/client';
 import { GET_USER_WITH_EMAIL } from '../../queries/database/databaseQueries';
 import { apolloDbClient } from '../../queries/apollo/apolloClients';
 import { UserContext } from '../../contexts/userContext';
+import { AlertContext } from '../../contexts/alertContext';
+import useCompositionColor from '../../hooks/useCompositionColor';
+import { PageContext } from '../../contexts/pageContext';
 
 interface props {
   firebaseUser: firebase.User
@@ -12,12 +15,18 @@ interface props {
 
 const LoginHandler = ({ firebaseUser }: props) => {
   const [user, setUser] = useContext(UserContext);
+  const [page, setPage] = useContext(PageContext);
+  const [alerts, setAlerts] = useContext(AlertContext);
+
   console.log("anonymous user");
 
   const { loading, error, data } = useQuery<databaseUser.GetUserWithEmailData>(GET_USER_WITH_EMAIL, { variables: { email: firebaseUser.isAnonymous ? "alice@prisma.io" : firebaseUser.email }, client: apolloDbClient });
   useEffect(() => {
     if (data) {
       setUser(prev => ({ ...prev, loggedInUser: data.getUserWithEmail }));
+      setPage(prev=>({...prev, user: firebaseUser}));
+      const alertItem: AlertItem = { header: `Hej ${data.getUserWithEmail.name}!`, text: "Välkommen till Kangaroo! Du är inloggad och klar att börja surfa.", color: useCompositionColor("green") }
+      setAlerts(prev => [...prev, alertItem]);
     }
   }, [data])
 
